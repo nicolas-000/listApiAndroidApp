f80630c9203ebe9864df2a14a7c2e398a2ce40bb
HowLonely <ellian.andre1203@gmail.com>
2023-12-18 22:16:17 -0300

    Añadiendo permisos para protocolo HTTP y correccion del parseo de JSON

--- app/src/main/java/cl/nicolas000/cardlisting/RegisteredCards.java
+++ app/src/main/java/cl/nicolas000/cardlisting/RegisteredCards.java
@@ -1,78 +1,92 @@
 package cl.nicolas000.cardlisting;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 public class RegisteredCards extends AppCompatActivity {
-    ListView listRegistered;
-    ArrayList<String> listCards;
-    RequestQueue data;
-    Handler mHandler=new Handler();
+    private ListView listRegistered;
+    private ArrayList<String> listCards;
+    private ArrayAdapter<String> adapter;
+    private RequestQueue data;
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered_cards);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        listRegistered = (ListView) findViewById(R.id.listRegistered);
+        listRegistered = findViewById(R.id.listRegistered);
+        listCards = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listCards);
+        listRegistered.setAdapter(adapter);
 
         data = Volley.newRequestQueue(this);
-        Refresh.run();
+
+        // Inicia la obtención de datos inmediatamente
+        mHandler.post(Refresh);
     }
 
-    public Runnable Refresh =new Runnable() {
+    private Runnable Refresh = new Runnable() {
         @Override
         public void run() {
             obtainData();
-            mHandler.postDelayed(this,1000);
+            mHandler.postDelayed(this, 1000);
         }
     };
 
-    public void obtainData()
-    {
-        String url="http://18.205.16.41/getAllCardData.php";
+    private void obtainData() {
+        String url = "http://18.205.16.41/getAllCardData.php";
 
-        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
-                System.out.println(response);
-                for (int i = 0; i < response.length(); i++) {
-                    try {
+            public void onResponse(JSONArray response) {
+                try {
+                    // Limpia la lista antes de agregar nuevos elementos
+                    listCards.clear();
+
+                    for (int i = 0; i < response.length(); i++) {
                         JSONObject responseObj = response.getJSONObject(i);
                         String idCard = responseObj.getString("id");
                         String uuidCard = responseObj.getString("uuid");
                         String registerDateCard = responseObj.getString("register_date");
                         listCards.add(idCard + " " + uuidCard + " " + registerDateCard);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
+
+                    // Notifica al adaptador que los datos han cambiado
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("test", "Error al procesar el JSON: " + e.getMessage());
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
+                Log.e("test", "Error en la solicitud HTTP: " + error.getMessage());
             }
         });
         data.add(request);
     }
-
 }
